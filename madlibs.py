3630a4dd66547a534e3e0852bfdff298fb7f2b1c
Khaleed <z5600243@ad.unsw.edu.au>
2025-07-08 11:14:30 +1000

    Converetd each game to have a function as a starter and fixed the main fi;e

--- madlibs.py
+++ madlibs.py
@@ -2,5 +2,5 @@ from madlibs_games import game_1, game_2, game_3, game_4
 import random
 
 if __name__ == "__main__":
-    m = random.choice([game_1, game_2, game_3, game_4])
-    m.madlib()
+    m = random.choice([game_1.madlibs, game_2.madlibs, game_3.madlibs, game_4.madlibs])
+    m()
