339d966d9be85b173686e920017924c794f1c11d
Khaleed <z5600243@ad.unsw.edu.au>
2025-07-08 10:53:15 +1000

    Created game 1 layout

---
+++ madlibs_games/game_1.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+super_hero = input("Super Hero: ")
+
+madlibs = f"Computer programming is so {adj}! It makes me so excited all the time. However i love to {verb1} much more than i love to code because {verb1} doesn't give me a headache.\
+ Finally, stay hydrated and {verb2} like you are a {super_hero}! KACHOW!"
+        
+print(madlibs)
